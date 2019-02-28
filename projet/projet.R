530fe366aa0fbfaa7fa5ee9d1396d820e3c01753
projet2018 <35642365+projet2018@users.noreply.github.com>
2019-02-28 18:32:07 +0100

    Update projet.R

--- projet/projet.R
+++ projet/projet.R
@@ -1,10 +1,5 @@
 library(readr)
 
-countries <- read_csv("C:/Users/joris/OneDrive/Documents/Master/Semestre_2/SEP/projet/countries.csv", 
-         col_types = cols(Agriculture = col_number(), 
-         `Arable (%)` = col_number(), `Coastline (coast/area ratio)` = col_number(), 
-         `Crops (%)` = col_number(), Industry = col_number(), 
-         `Net migration` = col_number(), `Phones (per 1000)` = col_number(), 
-         Service = col_number()))
+countries <- read_csv("C:/Users/joris/OneDrive/Documents/Master/Semestre_2/SEP/projet/countries.csv", locale = locale(decimal_mark = ","))
 
 View(countries)
