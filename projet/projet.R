1ef6e27b9731d0a3bf30f1e97fe8dfb3296eb37d
projet2018 <35642365+projet2018@users.noreply.github.com>
2019-02-28 19:24:57 +0100

    Update projet.R

--- projet/projet.R
+++ projet/projet.R
@@ -3,3 +3,11 @@ library(readr)
 countries <- read_csv("C:/Users/joris/OneDrive/Documents/Master/Semestre_2/SEP/projet/countries.csv", locale = locale(decimal_mark = ","))
 
 View(countries)
+
+typeof(countries)
+
+
+cols.dont.want <- c("Region", "Other (%)", "Climate", "Arable (%)", "Crops (%)", "Phones (per 1000)", "Area (sq. mi.)") # if you want to remove multiple columns
+
+data <- countries[, ! names(countries) %in% cols.dont.want, drop = F]
+data
