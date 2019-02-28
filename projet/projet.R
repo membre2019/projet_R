27a50fd602c854c54cbda8bb935b017fb399fb03
projet2018 <35642365+projet2018@users.noreply.github.com>
2019-02-28 20:00:14 +0100

    Update projet.R

--- projet/projet.R
+++ projet/projet.R
@@ -1,13 +1,17 @@
 library(readr)
 
+# Chargement des données
 countries <- read_csv("C:/Users/joris/OneDrive/Documents/Master/Semestre_2/SEP/projet/countries.csv", locale = locale(decimal_mark = ","))
-
 View(countries)
 
-typeof(countries)
-
-
-cols.dont.want <- c("Region", "Other (%)", "Climate", "Arable (%)", "Crops (%)", "Phones (per 1000)", "Area (sq. mi.)") # if you want to remove multiple columns
-
+# Suppression des colonnes les moins informatives
+cols.dont.want <- c("Population", "Region", "Other (%)", "Climate", "Arable (%)", "Crops (%)", "Phones (per 1000)", "Area (sq. mi.)") 
 data <- countries[, ! names(countries) %in% cols.dont.want, drop = F]
+data
+
+# Corrélation entre la migration et les autres variables
+cor(subset(data, select=-c(1)), data["Net migration"], use="complete.obs")
+
+# Suppression des lignes contenant des valeurs NA
+data = data[complete.cases(data), ]
 data
